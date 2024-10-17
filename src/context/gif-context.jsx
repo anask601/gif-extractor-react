c99bb743dec3cdf5eda9b6b60fa2a68f2f1650d1
anask601 <anask60155@gmail.com>
2024-10-17 14:59:21 +0530

    ✨ feat: fetch api implemented for giphy

--- src/context/gif-context.jsx
+++ src/context/gif-context.jsx
@@ -1,9 +1,31 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
 
 const GifContext = createContext();
 
 const GifProvider = ({ children }) => {
-  return <GifContext.Provider>{children}</GifContext.Provider>;
+  const [gifs, setGifs] = useState([]);
+  const [filter, setFilter] = useState("gifs");
+  const [favorites, setFavorites] = useState([]);
+  const gf = new GiphyFetch(import.meta.env.GIPHY_KEY);
+  return (
+    <GifContext.Provider
+      value={{
+        gf,
+        gifs,
+        setGifs,
+        filter,
+        setFilter,
+        favorites,
+      }}
+    >
+      {children}
+    </GifContext.Provider>
+  );
+};
+
+export const GifState = () => {
+  return useContext(GifContext);
 };
 
 export default GifProvider;
